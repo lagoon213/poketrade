f8a7d2ded14529f321502a400ac7d858220109ee
lagoon213 <standowicz.roksana@gmail.com>
2025-11-07 18:24:02 +0100

    setup tests for frontend + first test

---
+++ frontend/poketrade-frontend/poketrade-frontend/src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+﻿import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+test("renders homepage title", () => {
+    render(<App />);
+    const title = screen.getByText(/Vite \+ React/i);
+    expect(title).toBeInTheDocument();
+});
