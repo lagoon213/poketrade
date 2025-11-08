b870b5d53b66e401e825a0cdfcb593f7cf71b66e
lagoon213 <standowicz.roksana@gmail.com>
2025-11-08 19:39:41 +0100

    Added footer component (tsx file + css file)

--- frontend/poketrade-frontend/poketrade-frontend/src/App.tsx
+++ frontend/poketrade-frontend/poketrade-frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
+        <div className="main-page">
+            <h1>Ruil je Pokemon kaarten eenvoudig en veilig</h1>
+            <p>Vindt verzamelaars, maak afspraken<br />en breid je collectie uit</p>
+            <button>Maak account aan</button>
+            <div className="main-page-line"></div>
+            <h2>Openstaande trades</h2>
+        </div>
+        <Footer />
     </>
   )
 }
---
+++ frontend/poketrade-frontend/poketrade-frontend/src/components/Footer.tsx
@@ -0,0 +1,26 @@
+﻿import "./Footer.css"
+import Logo from "../assets/logo-PokeTrade.png"
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer-top-line"></div>
+
+            <div className="footer-triangle">
+                <div className="footer-navigation">
+                    <p>Asked questions</p>
+                    <p>Contact us</p>
+                    <p>Our story</p>
+                    <p>About us</p>
+                </div>
+            </div>
+            <div className="logo">
+                <img src={Logo} alt="poketrade logo"/>
+                <h1>poketrade</h1>
+            </div>
+            <div className="socialmedia-icons">
+
+            </div>
+        </div>
+    )
+}
