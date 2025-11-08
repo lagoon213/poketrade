b7448fd47a4ea1733e41efd6f6e2bbf1318667c7
lagoon213 <standowicz.roksana@gmail.com>
2025-11-08 20:22:09 +0100

    added tests for footer and navbar

---
+++ frontend/poketrade-frontend/poketrade-frontend/src/__tests__/Footer.test.tsx
@@ -0,0 +1,25 @@
+﻿import Footer from "../components/Footer";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+function renderWithRouter(ui: React.ReactElement) {
+    return render(<BrowserRouter>{ui}</BrowserRouter>);
+}
+
+test("renders footer logo", () => {
+    renderWithRouter(<Footer />);
+    const logo = screen.getByAltText(/poketrade/i);
+    expect(logo).toBeInTheDocument();
+});
+
+test("renders footer navigation links", () => {
+    renderWithRouter(<Footer />);
+    const faqLink = screen.getByText(/Asked questions/i);
+    const contactLink = screen.getByText(/Contact us/i);
+    const storyLink = screen.getByText(/Our story/i);
+    const aboutLink = screen.getByText(/About us/i);
+    expect(faqLink).toBeInTheDocument();
+    expect(contactLink).toBeInTheDocument();
+    expect(storyLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+});
---
+++ frontend/poketrade-frontend/poketrade-frontend/src/__tests__/Navbar.test.tsx
@@ -0,0 +1,25 @@
+﻿import { render, screen } from "@testing-library/react";
+import Navbar from "../components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+function renderWithRouter(ui: React.ReactElement) {
+    return render(<BrowserRouter>{ui}</BrowserRouter>);
+}
+
+test("renders logo", () => {
+    renderWithRouter(<Navbar />);
+    const logo = screen.getByAltText(/poketrade/i);
+    expect(logo).toBeInTheDocument();
+});
+
+test("renders navigation links", () => {
+    renderWithRouter(<Navbar />);
+    const homeLink = screen.getByText(/Home/i);
+    const tradesLink = screen.getByText(/Trades/i);
+    const collectionLink = screen.getByText(/Collection/i);
+    const loginLink = screen.getByText(/Login/i);
+    expect(homeLink).toBeInTheDocument();
+    expect(tradesLink).toBeInTheDocument();
+    expect(collectionLink).toBeInTheDocument();
+    expect(loginLink).toBeInTheDocument();
+}
