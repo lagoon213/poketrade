047d883527b408a77744d926b639e307c0bee31e
lagoon213 <standowicz.roksana@gmail.com>
2025-11-08 17:35:58 +0100

    changed the navbar code to be a component

--- frontend/poketrade-frontend/poketrade-frontend/src/App.tsx
+++ frontend/poketrade-frontend/poketrade-frontend/src/App.tsx
@@ -1,23 +1,10 @@
 import './App.css'
-import pokeTradeLogo from './assets/logo-PokeTrade.png'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      <div>
-          <div className="navbar">
-              <div className="navbar-brand">
-                <img src={pokeTradeLogo} className="logo-poketrade" alt="PokeTrade logo" />
-                <h1>POKETRADE</h1>
-              </div>
-              <div className="navbar-buttons">
-                <h2>Home</h2>
-                <h2>Trades</h2>
-                <h2>Collection</h2>
-                <button>Login</button>
-              </div>
-          </div>
-      </div>
+      <Navbar />
     </>
   )
 }
---
+++ frontend/poketrade-frontend/poketrade-frontend/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+﻿import './Navbar.css'
+import pokeTradeLogo from '../assets/logo-PokeTrade.png'
+
+export default function Navbar() {
+    return (
+            <div>
+                <div className="navbar">
+                    <div className="navbar-brand">
+                        <img src={pokeTradeLogo} className="logo-poketrade" alt="PokeTrade logo" />
+                        <h1>POKETRADE</h1>
+                    </div>
+                    <div className="navbar-buttons">
+                        <h2>Home</h2>
+                        <h2>Trades</h2>
+                        <h2>Collection</h2>
+                        <button>Login</button>
+                    </div>
+                </div>
+            </div>
+    )
+}
